1c027644b7eef9a39326df9433409744712444fd
root <root@LAPTOP-ALPNPJG3.localdomain>
2022-11-06 10:48:50 +0530

    fixed loading, fixed serverside styled components not loading

--- next_port/pages/_document.jsx
+++ next_port/pages/_document.jsx
@@ -1,19 +1,70 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// import { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-    return (
-        <Html>
-            <Head>
+// export default function Document() {
+//     return (
+//         <Html>
+//             <Head>
 
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
+//                 <link rel="preconnect" href="https://fonts.googleapis.com" />
+//                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+//                 <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
 
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+//             </Head>
+//             <body>
+//                 <Main />
+//                 <NextScript />
+//             </body>
+//         </Html>
+//     );
+// }
+
+
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Space+Grotesk:wght@500&display=swap" rel="stylesheet" />
+
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
 }
